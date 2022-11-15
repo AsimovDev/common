ddcfbacc5e5d5fbcc1d8290293788134539a6fef
Alonso-20 <alonso_ca_30@hotmail.com>
2022-11-14 19:25:37 -0500

    first commit

---
+++ src/main/java/com/asimov/common/application/Notification.java
@@ -0,0 +1,31 @@
+package com.asimov.common.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Notification {
+    private List<Error> errors = new ArrayList<>();
+    private final String SEPARETOR = "||";
+
+    public void addError(String message){
+        addError(message, null);
+    }
+
+    public void addError(String message, Exception cause){
+        errors.add(new Error(message, cause));
+    }
+
+    public List<Error> getErrors(){
+        return errors;
+    }
+
+    public String errorMessage(){
+        return errors.stream().map(e -> e.getMessage())
+                .collect(Collectors.joining(SEPARETOR + ""));
+    }
+
+    public boolean hasErrors(){
+        return !errors.isEmpty();
+    }
+}
---
+++ src/main/java/com/asimov/common/application/ResultType.java
@@ -0,0 +1,6 @@
+package com.asimov.common.application;
+
+public enum ResultType {
+    SUCCESS,
+    FAILURE
+}
